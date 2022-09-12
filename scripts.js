c29dc9e774dcf0e976e744952de897f4077962aa
roysaucedo <roy.a.saucedo@gmail.com>
2022-09-12 12:09:42 -0700

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let billTotal = document.getElementById('bill');
+let tip5Selected = document.getElementById('5-percent');
+let tip10Selected = document.getElementById('10-percent');
+let tip15Selected = document.getElementById('15-percent');
+let tip20Selected = document.getElementById('10-percent');
+let peopleTotal = document.getElementById('num-people');
+let tipAmountTotal = document.getElementById('tip-amount');
+let tipPerPersonTotal = document.getElementById('tip-per-person');
+
+console.log(billTotal.value)
+
+// Formula = Bill 
+
+function calcTip(){
+    
+}
